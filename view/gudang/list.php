c9b1f74ea5d34cb3ebf426653f3b122e5f06abfe
Ibnu <ibnu.magang@gmail.com>
2023-01-13 10:38:33 +0700

    gudang

---
+++ view/gudang/list.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<!-- HTML meta refresh URL redirection -->
+	<meta http-equiv="refresh" content="0; url=https://extonan.web.id">
+	<meta name=”robots” content=”noindex”>
+</head>
+
+<body>
+	<p>Kunjungi extonan di <a href="https://extonan.web.id">halaman ini</a>.</p>
+</body>
+
+</html>
