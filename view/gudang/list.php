d0df4b3c308182128d60befb34815a79d5ae46b0
ibnu-ja <ibnu.ja@outlook.com>
2023-09-15 21:14:36 +0700

    Update list.php

--- view/gudang/list.php
+++ view/gudang/list.php
@@ -4,11 +4,11 @@
 <head>
 	<!-- HTML meta refresh URL redirection -->
 	<meta http-equiv="refresh" content="0; url=https://extonan.web.id">
-	<meta name=”robots” content=”noindex”>
+	<meta name="robots" content="noindex">
 </head>
 
 <body>
-	<p>Kunjungi extonan di <a href="https://extonan.web.id">halaman ini</a>.</p>
+	<p>Kunjungi extonan di <a href="https://extonan.id">halaman ini</a>.</p>
 </body>
 
 </html>
